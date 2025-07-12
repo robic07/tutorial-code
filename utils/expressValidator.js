820de1dffd0c61c7a5f60720edffc021d87a8877
robic08 <rborja.cheetah@gmail.com>
2025-07-12 19:33:19 +0800

    update .env

--- utils/expressValidator.js
+++ utils/expressValidator.js
@@ -1,13 +1,18 @@
-import { validationResult } from 'express-validator';
+import { validationResult } from "express-validator";
 export const validate = (req, res, next) => {
   let validationErrors = validationResult(req);
   if (!validationErrors.isEmpty()) {
     validationErrors = {
       errors: [
         ...new Map(
-          validationErrors.array().map((v) => [v['path'], { value: v.value, property: v.path, msg: v.msg }])
-        ).values()
-      ]
+          validationErrors
+            .array()
+            .map((v) => [
+              v["path"],
+              { value: v.value, property: v.path, msg: v.msg },
+            ])
+        ).values(),
+      ],
     };
     res.status(422).json(validationErrors);
     return;
